181113225f3b7c1b77a56c4195aa4c7d7bafcf8d
Benjamin Nieuwenhuijzen <54937850+BenjaminNieuwenhuijzen@users.noreply.github.com>
2020-05-22 20:44:33 +0200

    Add files via upload
    Adding Bill.cs

---
+++ Model/Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Bill
+    {
+        public int ID { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public double Price { get; }
+
+        public Bill(int iD, string firstName, string lastName, double price)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Price = price;
+        }
+    }
+}
