06e4793faf1016bd8192a06074438c344621a0cc
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-05-22 15:37:41 +0200

    Drink_Service gamaakt

---
+++ Logic/Drink_Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dao;
+using Model;
+
+namespace Logic
+{
+    public class Drink_Service
+    {
+        Drink_DAO drink_db = new Drink_DAO();
+
+        //Get a list of all the drinks from the DAO
+        public List<Drink> GetAllDrinks()
+        {
+            return null;
+        }
+
+        //Add a new drink
+        public void AddNewDrink(Drink drink)
+        {
+
+        }
+
+        //Remove a drink
+        public void RemoveDrink(Drink drink)
+        {
+
+        }
+
+        //Modify the properties of a drink
+        public void ModifyDrink(Drink drink)
+        {
+
+        }
+    }
+}
