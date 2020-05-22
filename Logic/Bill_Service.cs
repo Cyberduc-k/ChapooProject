e0ef86e950533cf8b5f71f2c5d50b99bf1a01646
Benjamin Nieuwenhuijzen <54937850+BenjaminNieuwenhuijzen@users.noreply.github.com>
2020-05-22 20:39:54 +0200

    Add files via upload
    Adding Bill_Service.cs

---
+++ Logic/Bill_Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElChapoo
+{
+    public class Bill_Service
+    {
+        Bill_DAO bill_db = new Bill_DAO();
+
+        public List<Bill> GetAllBills()
+        {
+            // @TODO
+        }
+
+        public List<Bill> GetBill()
+        {
+            // @TODO
+        }
+
+        public List<Bill> AddBill()
+        {
+            // @TODO
+        }
+
+        public List<Bill> DeleteBill()
+        {
+            // @TODO
+        }
+
+        public List<Bill> UpdateBill()
+        {
+            // @TODO
+        }
+    }
+}
