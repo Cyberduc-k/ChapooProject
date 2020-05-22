50c765bd287572da0d7c35818e445ea45d07be0c
Benjamin Nieuwenhuijzen <54937850+BenjaminNieuwenhuijzen@users.noreply.github.com>
2020-05-22 21:34:20 +0200

    Add files via upload
    Aanpassing van de classen van Bill.cs

--- Model/Bill.cs
+++ Model/Bill.cs
@@ -12,13 +12,19 @@ namespace Model
         public string FirstName { get; }
         public string LastName { get; }
         public double Price { get; }
+        public Medewerker medewerker { get; }
+        public Tafel tafel { get; }
+		public Dish dish { get; }
+		public Drink drink { get; }
 
-        public Bill(int iD, string firstName, string lastName, double price)
+        public Bill(int iD, string firstName, string lastName, double price, Medewerker medewerker, Tafel tafel)
         {
             ID = iD;
             FirstName = firstName;
             LastName = lastName;
             Price = price;
+            Medewerker = medewerker;
+            Tafel = tafel;
         }
     }
 }
