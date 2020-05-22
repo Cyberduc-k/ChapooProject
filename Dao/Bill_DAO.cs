642dcba6d377eba475e7c52faeda82f170e72774
Benjamin Nieuwenhuijzen <54937850+BenjaminNieuwenhuijzen@users.noreply.github.com>
2020-05-22 20:38:10 +0200

    Add files via upload
    Adding Bill Dao/Model/Service
    Adding Order Service

---
+++ Dao/Bill_DAO.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using Model;
+
+namespace Dao
+{
+    public class Bill_DAO : Base
+    {
+
+        public List<Bill> Db_Get_All_Bills()
+        {
+            // @TODO
+        }
+
+        private List<Bill> ReadTables()
+        {
+            // @TODO
+        }
+
+        public void editBill()
+        {
+            // @TODO
+        }
+
+        public void addBill()
+        {
+            // @TODO
+        }
+
+        public void deleteBill()
+        {
+            // @TODO
+        }
+    }
+}
---
+++ Dao/Bill_Model.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using Service;
+
+namespace Model
+{
+    public class Bill_Service
+    {
+        Bill_DAO bill_db = new Bill_DAO();
+
+        public List<Bill> GetBill()
+        {
+            // @TODO
+        }
+
+        public void editBill()
+        {
+            // @TODO
+        }
+
+        public void addBill()
+        {
+            // @TODO
+        }
+
+        public void deleteBill()
+        {
+            // @TODO
+        }
+    }
+}
