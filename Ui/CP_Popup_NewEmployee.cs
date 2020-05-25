62eee51207488ecdc5962f7696a2ff085c89dc1a
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-05-25 14:31:15 +0200

    You cant add new employee without filling things in

--- Ui/CP_Popup_NewEmployee.cs
+++ Ui/CP_Popup_NewEmployee.cs
@@ -14,6 +14,10 @@ namespace Ui
 {
     public partial class CP_Popup_NewEmployee : Form
     {
+        private bool firstNameFilledIn = false;
+        private bool lastNameFilledIn = false;
+        private bool passwordFilledIn = false;
+
         public CP_Popup_NewEmployee()
         {
             InitializeComponent();
@@ -45,17 +49,51 @@ namespace Ui
             else
                 employeeType = EmployeeType.Chef;
 
-            //@TODO Handle errors
-            if (firstName == "" || lastName == "" || password == null)
-                return;
-
-            //employeeService.AddEmployee(new Employee(1, "Bram", "Sierhuis", new DateTime(2001, 5, 16), new DateTime(2020, 5, 20), Gender.Male, "2343", EmployeeType.Chef));
             employeeService.AddEmployee(new Employee(1, firstName, lastName, birthDate, employment, gender, password, employeeType));
         }
 
         private void CP_PopupNewEmployee_btnCancel_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void CP_PopopNewEmployee_txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+            if (CP_PopopNewEmployee_txtFirstName.Text != "")
+                firstNameFilledIn = true;
+            else
+                firstNameFilledIn = false;
+
+            UpdateOKbtn();
+        }
+
+        private void CP_PopupNewEmployee_txtLastName_TextChanged(object sender, EventArgs e)
+        {
+            if (CP_PopupNewEmployee_txtLastName.Text != "")
+                lastNameFilledIn = true;
+            else
+                lastNameFilledIn = false;
+
+            UpdateOKbtn();
+        }
+
+        private void CP_PopopNewEmployee_txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (CP_PopopNewEmployee_txtPassword.Text != "")
+                passwordFilledIn = true;
+            else
+                passwordFilledIn = false;
+
+            UpdateOKbtn();
+        }
+
+        private void UpdateOKbtn()
+        {
+
+            if (firstNameFilledIn && lastNameFilledIn && passwordFilledIn)
+                CP_Popup_NewEmployee_btnOK.Enabled = true;
+            else
+                CP_Popup_NewEmployee_btnOK.Enabled = false;
         }
     }
 }
