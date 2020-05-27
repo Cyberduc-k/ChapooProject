c689ba5e025ad1d70e51bb5585dc9fb0d5e19c9c
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-05-28 00:16:59 +0200

    Created a parent form for ok/cancel popups

---
+++ Ui/CP_Popup_Parent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logic;
+
+namespace Ui
+{
+    public abstract partial class CP_Popup_Parent : Form
+    {
+        public CP_Popup_Parent()
+        {
+            InitializeComponent();
+        }
+
+        private void CP_Popup_Parent_btnOK_Click(object sender, EventArgs e)
+        {
+            OnClickOK(sender, e);
+        }
+
+        private void CP_Popup_Parent_btnCancel_Click(object sender, EventArgs e)
+        {
+            OnClickCancel(sender, e);
+        }
+
+        public abstract void OnClickOK(object sender, EventArgs e);
+
+        public abstract void OnClickCancel(object sender, EventArgs e);
+
+        public void LoadIcon(string icon)
+        {
+            try
+            {
+                Icon = new Icon(icon);
+            }
+            catch (Exception e)
+            {
+                ErrorHandler.Instance.HandleError("Het icoon con niet ingeladen worden", "Icoon niet gevonden", e);
+            }
+        }
+    }
+}
